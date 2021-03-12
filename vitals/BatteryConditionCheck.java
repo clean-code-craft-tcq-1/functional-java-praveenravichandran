deced130b5c57b7c5fc88c28d2382a1c03ec07a2
Praveen <ravichandran.praveen@in.bosch.com>
2021-03-12 11:09:41 +0530

    CCN rectified

--- vitals/BatteryConditionCheck.java
+++ vitals/BatteryConditionCheck.java
@@ -19,34 +19,47 @@ public class BatteryConditionCheck {
 			MessageUtil.printMessage(MessageConstants.BREACH, MessageConstants.CHARGE_RATE, MessageConstants.HIGH);
 			return false;
 		}
-		checkForWarning(chargeRate, 0, 0.8f, MessageConstants.CHARGE_RATE, 5);
+		checkForMaxLevelWarning(chargeRate, 0.8f, MessageConstants.CHARGE_RATE, 5);
 		return true;
 	};
 	static Function<Float, Function<Float, Boolean>> socCheck = (soc) -> {
-		if (soc < 20 || soc > 80) {
-			MessageUtil.printMessage(MessageConstants.BREACH, MessageConstants.SOC,
-					soc > 80 ? MessageConstants.HIGH : MessageConstants.LOW);
+		if (soc < 20) {
+			MessageUtil.printMessage(MessageConstants.BREACH, MessageConstants.SOC, MessageConstants.LOW);
 			return null;
 		}
-		checkForWarning(soc, 20, 80, MessageConstants.SOC, 5);
+		if (soc > 80) {
+			MessageUtil.printMessage(MessageConstants.BREACH, MessageConstants.SOC, MessageConstants.HIGH);
+			return null;
+		}
+		checkForMinLevelWarning(soc, 20, 80, MessageConstants.SOC, 5);
+		checkForMaxLevelWarning(soc, 80, MessageConstants.SOC, 5);
 		return chargeRateCheck;
 	};
 
 	static Function<Float, Function<Float, Boolean>> temperatureCheck(float temperature) {
-		if (temperature < 0 || temperature > 45) {
-			MessageUtil.printMessage(MessageConstants.BREACH, MessageConstants.TEMPERATURE,
-					temperature > 45 ? MessageConstants.HIGH : MessageConstants.LOW);
+		if (temperature < 0) {
+			MessageUtil.printMessage(MessageConstants.BREACH, MessageConstants.TEMPERATURE, MessageConstants.LOW);
 			return null;
 		}
-		checkForWarning(temperature, 0, 45, MessageConstants.TEMPERATURE, 5);
+		if (temperature > 45) {
+			MessageUtil.printMessage(MessageConstants.BREACH, MessageConstants.TEMPERATURE, MessageConstants.HIGH);
+			return null;
+		}
+		checkForMinLevelWarning(temperature, 0, 45, MessageConstants.TEMPERATURE, 5);
+		checkForMaxLevelWarning(temperature, 45, MessageConstants.TEMPERATURE, 5);
 		return socCheck;
 	}
 
-	static void checkForWarning(float value, float min, float max, String type, float deltaPercentage) {
+	static void checkForMinLevelWarning(float value, float min, float max, String type, float deltaPercentage) {
 		float delta = (deltaPercentage / max) * 100;
-		if (type != MessageConstants.CHARGE_RATE && value <= (min + delta)) {
+		if (value <= (min + delta)) {
 			MessageUtil.printMessage(MessageConstants.WARNING, type, MessageConstants.LOW);
-		} else if (value >= (max - delta)) {
+		}
+	}
+
+	static void checkForMaxLevelWarning(float value, float max, String type, float deltaPercentage) {
+		float delta = (deltaPercentage / max) * 100;
+		if (value >= (max - delta)) {
 			MessageUtil.printMessage(MessageConstants.WARNING, type, MessageConstants.HIGH);
 		}
 	}
